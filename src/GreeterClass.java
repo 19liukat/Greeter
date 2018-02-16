981db130ca8ea1ec5111ca8749b7982fb079a4a9
Unknown <19liukat@elmbrookstudents.org>
2018-02-16 11:50:25 -0600

    Greeter Class

---
+++ src/GreeterClass.java
@@ -0,0 +1,15 @@
+
+public class GreeterClass {
+
+	private String name;
+	private int age;
+	
+	public GreeterClass(){
+		name = "Katherine Liu";
+		age = 16;
+	}
+	public GreeterClass(String xName, int xAge){
+		name = xName;
+		age = xAge;
+	}
+}
