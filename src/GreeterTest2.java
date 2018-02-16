76b21a592974f9648103160c2d676610140ebc68
Unknown <19liukat@elmbrookstudents.org>
2018-02-16 12:02:14 -0600

    Test Class 2 (Array)

---
+++ src/GreeterTest2.java
@@ -0,0 +1,10 @@
+
+public class GreeterTest2 {
+
+	public static void main(String[] args){
+		GreeterClass[] bob = new GreeterClass[7];
+		for(GreeterClass temp: bob){
+			System.out.println(temp.toString());
+		}
+	}
+}
